0f865b6a204f391a8240afbc0ff0a83c626a867b
Bernard Tatin <bernard.tatin@outlook.fr>
2016-10-27 21:31:49 +0200

    moving *.sh in bin

--- z390/exec.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ici=$(dirname $0)
-source ${ici}/shlib.sh
-
-# exec execute 390 load module
-rm -fv $1.log $1.err $1.sta $1.tr*
-${ici}/ez390.sh $@ ||
-	onerror 1 "see errors on ez390 generated log file and console"
